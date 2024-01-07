71eb9ad053f8d2a9033aae5c209d2b98b30b12ec
ILLAHIB <155870604+ILLAHIB@users.noreply.github.com>
2024-01-07 10:57:07 +0500

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+async function startCalu() {
+    console.log(`Mini Calculator \n`);
+    const userInput = await inquirer.prompt([
+        {
+            type: "number",
+            name: "num1",
+            message: "Enter your first number"
+        },
+        {
+            type: "number",
+            name: "num2",
+            message: "Enter Your second Number"
+        },
+        {
+            type: "list",
+            name: "operator",
+            choices: ["Add", "Sub", "Multiply", "Divide"],
+            message: "Enter Your operator"
+        }
+    ]);
+    const userOp = userInput.operator;
+    const firstNum = userInput.num1;
+    const secondNum = userInput.num2;
+    if (userOp == "Add") {
+        console.log(`\n The answwer of ${firstNum} + ${secondNum} = ${firstNum + secondNum}`);
+    }
+    else if (userOp == "Sub") {
+        console.log(`\n The answer of ${firstNum} - ${secondNum} = ${firstNum - secondNum}`);
+    }
+    else if (userOp == "Multiply") {
+        console.log(`\n The answer of ${firstNum} x ${secondNum} = ${firstNum * secondNum}`);
+    }
+    else if (userOp == "Divide") {
+        console.log(`\n The answer of ${firstNum} / ${secondNum} = ${firstNum / secondNum}`);
+    }
+}
+startCalu();
